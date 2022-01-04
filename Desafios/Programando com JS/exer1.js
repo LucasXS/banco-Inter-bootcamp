1d1e0d56a782498019c867c58ed52174918475c2
lucas <contato_lucassantos@outlook.com>
2022-01-04 01:37:07 -0300

    Pequenas correções

--- Desafios/Programando com JS/exer1.js
+++ Desafios/Programando com JS/exer1.js
@@ -1,14 +1,6 @@
-let lines = gets().split('\n');
+let lines = gets(10000).split('\n');
 
 var N = parseInt(lines.shift());
 for (let i = 1; i <=N; i++) {
-	if (i %2 === 0) print(`${i}^2=${Math.pow(i, 2)}`);    //escreva sua lógica nos espaços em branco
-}
-
-
-let lines = gets().split('\n');
-
-var N = parseInt(lines.shift());
-for (let i = 1; i <=N; i++) {
-	if (i %2 === 0) print(i, + ''+ `${Math.pow(i, 2)}`);    //escreva sua lógica nos espaços em branco
+	if (i %2 === 0) console.log(`${i}^2 = ${i**2}`);    //escreva sua lógica nos espaços em branco
 }
---
+++ Desafios/Programando com JS/exer5.js
@@ -0,0 +1,6 @@
+let lines = gets().split("\n");
+let line = lines.shift().split(' ');
+
+//continue o seu código aqui
+let result = line[0] * line[1];
+console.log(Number(result));
