c628158298f9157a4a3004c10e2612496145e8a0
lucas <contato_lucassantos@outlook.com>
2022-01-03 21:33:51 -0300

    5 Desafios com JS.

---
+++ Desafios/Programando com JS/exer3.js
@@ -0,0 +1,11 @@
+let lines = gets().split("\n");
+
+let n = parseInt(lines.shift());
+//escreva o seu código aqui
+
+const frase = 'LIFE IS NOT A PROBLEM TO BE SOLVED';
+const result = frase.substring(0, n);
+
+console.log(result);
+
+
---
+++ Desafios/Programando com JS/exer4.js
@@ -0,0 +1,11 @@
+let lines = gets().split("\n");
+
+let line = lines.shift().split(' ');
+
+//escreva aqui o seu código
+let a = parseInt(line[0]);
+let b = parseInt(line[1]);
+let c = parseInt(line[2]);
+let d = parseInt(line[3]);
+
+console.log((a+b+c+d)-3);
